fa436bcb14684ee433b9945cf1c8e5dea0af8612
drkrieger <eplyushko@gmail.com>
2018-02-10 15:40:23 -0800

    epmployee and capitain models

---
+++ Fish-Registration/Models/Captain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fish_Registration.Models
+{
+    public class Captain
+    {
+       public int ID { get; set; }
+       public string FirstName { get; set;}
+       public string LastName { get; set;}
+    }
+}
