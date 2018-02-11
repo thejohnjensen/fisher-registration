a2a7f754420e763f5dd33a8701e7560fd584c127
drkrieger <eplyushko@gmail.com>
2018-02-10 16:17:42 -0800

    joining tables added

---
+++ Fish-Registration/Models/CaptainVessel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fish_Registration.Models
+{
+    public class CaptainVessel
+    {
+        public int ID { get; set; }
+
+        public Captain CaptainID { get; set; }
+        public Vessel VesselID { get; set; }
+    }
+}
---
+++ Fish-Registration/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fish_Registration.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public CaptainVessel CaptainVesselID { get; set; }
+        
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+    }
+}
